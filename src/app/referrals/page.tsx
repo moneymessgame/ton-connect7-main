6f5c68b1e5f8f4e08aba050e3ccc766748a7c84a
Irine7 <irine7@yandex.ru>
2024-09-05 16:21:25 +0200

    feat: invite friends

--- src/app/referrals/page.tsx
+++ src/app/referrals/page.tsx
@@ -1,22 +1,48 @@
-// src/app/referrals/page.tsx
 'use client';
 
 import React from 'react';
+import { useTranslations } from 'next-intl';
+
 import RefLink from '@/components/RefLink';
 import ReferralsList from '@/components/ReferralsList';
 import { useUser } from '@/contexts/UserContext';
+import { CardSpecial } from '@/components/InviteAFriend/CardSpecial';
+import { Content } from '@/components/Kit';
+import { Heading } from '@/components/Heading';
+
+import styles from './referrals.module.scss';
 
 const Referrals: React.FC = () => {
-  const { user, loading } = useUser();
+	const t = useTranslations();
+	const { user, loading } = useUser();
 
-  if (loading) return <div>Loading...</div>;
+	if (loading) return <div>Loading...</div>;
 
-  return (
-    <div>
-      <RefLink />
-      <ReferralsList />
-    </div>
-  );
+	return (
+		<Content>
+			<Heading
+				title={t('friends.title')}
+				txt={t('friends.txt')}
+				top={
+					<img
+						src="/gift.png"
+						width={732}
+						height={796}
+						alt="Gift"
+						className={styles.headingGift}
+					/>
+				}
+			/>
+			<div>
+				<RefLink />
+				<ReferralsList />
+				<CardSpecial
+					title={t('friends.title')}
+					description={t('friends.description')}
+				/>
+			</div>
+		</Content>
+	);
 };
 
 export default Referrals;
---
+++ src/components/InviteAFriend/CardSpecial.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './cards.module.scss';
+
+interface CardSpecialProps {
+	title: string;
+	description: string;
+}
+
+export const CardSpecial = ({ title, description }: CardSpecialProps) => {
+	return (
+		<div className={styles.special}>
+			<img src="/gift.png" width={732} height={796} alt={title} />
+			<div>
+				<h3>{title}</h3>
+				<span>
+					<strong>+10000</strong> {description}
+				</span>
+			</div>
+		</div>
+	);
+};
